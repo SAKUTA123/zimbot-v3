ff9a7d39307b666957b338d5fd45a2e45341d84a
Profile <91337596+SAKUTA123@users.noreply.github.com>
2023-02-28 01:49:31 +0700

    Update config.js
    Signed-off-by: Profile <91337596+SAKUTA123@users.noreply.github.com>

--- config.js
+++ config.js
@@ -37,14 +37,14 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['27634090203','']
-global.premium = ['27634090203','918188019676']
-global.botname = 'ZIM BOT INC'
-global.ownername = 'DRIPS'
-global.group1 = 'https://chat.whatsapp.com/EFsb8RCXV4jLEFk4eAcA1A'
-global.group2 = 'https://chat.whatsapp.com/HSfcYU13g5C8GxINWwSWoA'
-global.packname = 'ZIM BOT INC'
-global.ytchannel = 'https://youtu.be/ww4z2m3uORU'
+global.owner = ['6287760550924','']
+global.premium = ['6287760550924','6287760550924']
+global.botname = 'SOUTA-MD'
+global.ownername = 'leon'
+global.group1 = 'https://chat.whatsapp.com/CoBuuoNT1vC5xyBuyUBd5f'
+global.group2 = 'https://chat.whatsapp.com/CoBuuoNT1vC5xyBuyUBd5f'
+global.packname = 'SOUTA-MD'
+global.ytchannel = 'https://youtube.com/@leonnob2820'
 global.monayawal = '100000'
 global.darahawal = '100'
 global.besiAwal = '15'
@@ -65,7 +65,7 @@ global.f3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.docu
 global.f4 = 'application/zip'
 global.f5 = 'application/pdf'
 global.f6 = 'application/vnd.android.package-archive'
-global.author = 'ZIM BOT INC'
+global.author = 'Leon'
 global.flaming = 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text='
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
